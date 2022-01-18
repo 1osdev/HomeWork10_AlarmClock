d3a124fe6d7f98c60736eeb9697495cfae756342
DMITRY SMIRNOV <smirnoff145@gmail.com>
2022-01-19 00:14:00 +0300

    project Created

---
+++ homeWork10/homeWork10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork10
+//
+//  Created by Дмитрий Смирнов on 19.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
